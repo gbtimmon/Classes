5317633ad49f1682887f0b752663e56e0952d20c
gbtimmon <gbtimmon@ncsu.edu>
2016-03-21 23:35:42 -0400

    asdsad

--- CSC501/HW3/connection.c
+++ CSC501/HW3/connection.c
@@ -7,9 +7,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-Connection Connection_new( const char * h, int p){
+Connection Connection_new( const char * h, int p, int t){
     Connection c = malloc( sizeof ( struct connection ) ); 
     c->port = p;
+    c->type = t; 
     if( h ) c->host = malloc( sizeof( char* ) * strlen(h) ); 
     if( h ) strcpy( c->host, h); 
     return c; 
@@ -24,24 +25,18 @@ void Connection_free( Connection c ){
 
 void Connection_send( Connection connection, int target, int socket ) {
     Socket_sendi(socket, MSG_TYPE_CONNECTION);
-    Socket_sendi(socket, target); 
+    Socket_sendi(socket, connection->type); 
     Socket_sends(socket, connection->host);
     Socket_sendi(socket, connection->port);
 }
 
 void Connection_recv(char ** s){
+     int type = atoi(strsep(s,"\n"));
 
-    Connection c = malloc(sizeof(struct connection)); 
 
-    p->initial_count   = atoi(strsep( s, "\n" ));
-    p->current_count   = atoi(strsep( s, "\n" ));
-    p->signature_count = atoi(strsep( s, "\n" ));
-    p->signature       = malloc( sizeof(int) * p->signature_count );
 
-    for(int i = 0; i < p->signature_count; i++ )
-        p->signature[i] = atoi(strsep( s, "\n" ));
 
-    return p;
+     return Connection_new( 
 }
 
 
