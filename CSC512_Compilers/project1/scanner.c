0e1829968a9bfadaeb32eed6391237194484cde8
gbtimmon <gbtimmon@ncsu.edu>
2016-08-30 12:07:02 -0400

    autosave

--- CSC512_Compilers/project1/scanner.c
+++ CSC512_Compilers/project1/scanner.c
@@ -1,6 +1,7 @@
 #ifndef _SCANNER_C_
 #define _SCANNER_C_
 #include <stdio.h>
+#include <stdlib.h>
 
 const char* reserved_words[12] = {
     "int",
@@ -16,6 +17,17 @@ const char* reserved_words[12] = {
     "binary",
     "decimal"
 };
+
+int charIn( const char c, char * string ) {
+    char * i = string; 
+    while( *i != '\0' ){
+        if( *i == c )
+           return 1;
+        i++;
+    }
+    return 0; 
+};
+
 
 /**
     STATES
@@ -89,6 +101,11 @@ void Buffer_rewind( Buffer b ) {
     b.index = 0; 
 };
 
+void Buffer_emit( Buffer b ) { 
+   b.stack[ b.index + 1 ] = '\0';
+   printf( "%s\n", b.stack );
+};
+
 Buffer token_buffer = { 0, 0, "" };
 
 int isAlpha( const char c ) { 
@@ -102,22 +119,68 @@ int isNumeric( const char c ) {
 };
 
 
+#define CONSUME() { \
+      cur = getc(stdin);\
+      if( cur == EOF ) \
+          continue_loop = 1; \
+      continue; \
+      }
+         
+
+void Print_state() {
+    switch( state ) {
+    case STATE_NEWTOK :
+         printf(" State : STATE_NEWTOK \n");
+    break;
+    case STATE_IDENT :
+         printf(" State : STATE_IDENT  \n");
+    break;
+    case STATE_NUMBER : 
+         printf(" State : STATE_NUMBER  \n");
+    break; 
+    case STATE_SYMBOL : 
+         printf(" State : STATE_SYMBOL  \n");
+    break;
+    case STATE_STRING : 
+         printf(" State : STATE_STRING  \n");
+    break;
+    case STATE_MSTMT:
+         printf(" State : STATE_MSTMT \n");
+    break;
+    }
+}
+    
+int MAX_ITER = 35; 
+int CUR_ITER = 0; 
 int main( int argc, char ** argp, char ** envp ) { 
 
-    char buffer [1024], 
-         cur; 
+    char cur; 
+    int continue_loop = 0; 
 
-    while( ( cur = getc( stdin ) ) != EOF ) { 
-    
+    cur = getc( stdin );
+
+    if (  cur == EOF ) {
+        fprintf(stderr, "No input read\n"); 
+        exit(1); 
+    }
+  
+    while( continue_loop == 0  && CUR_ITER++ < MAX_ITER ) { 
+        Print_state();
         printf(" %c\n", cur);
 
         switch( state ) {
         case STATE_NEWTOK :
             if( isNumeric( cur ) ) { 
-
-            else if( isAlpha( cur ) ) { 
+                state = STATE_NUMBER;
+            } else if( isAlpha( cur ) ) { 
                 state = STATE_IDENT;
-            }           
+            } else if( cur == '#' ){
+                state = STATE_MSTMT;
+            } else if( cur == '"' || cur == '\'' ){
+                state = STATE_STRING;
+            } else if( charIn( cur, "+-/*()") ) {
+                state = STATE_SYMBOL;
+            }
         break;
         case STATE_IDENT :
 
@@ -132,7 +195,13 @@ int main( int argc, char ** argp, char ** envp ) {
  
         break;
         case STATE_MSTMT:
-
+            Buffer_write( token_buffer, cur );
+            if( cur == '\n'){
+                state = STATE_NEWTOK;
+                Buffer_emit( token_buffer );         
+                Buffer_reset( token_buffer ); 
+            }
+            CONSUME();
         break;
         }
     }
